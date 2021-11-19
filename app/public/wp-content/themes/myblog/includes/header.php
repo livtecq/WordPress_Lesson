638b958b93ed72d3280844257781f5c0faef8cb8
livtecq <nishiki1202@gmail.com>
2021-11-19 11:55:39 +0900

    仕上げ

--- app/public/wp-content/themes/myblog/includes/header.php
+++ app/public/wp-content/themes/myblog/includes/header.php
@@ -1,7 +1,8 @@
 <?php wp_body_open(); ?>
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <!-- ホームに戻るURLに/の使用は極力避ける。home_urlを使用する。 -->
+      <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
@@ -20,7 +21,8 @@
         <ul class="navbar-nav ml-auto">
           <?php foreach ($menu_items as $item): ?>
           <li class="nav-item">
-            <a class="nav-link" href="<?php echo $item->url; ?>"><?php echo $item->title; ?></a>
+            <!-- 画面表示のときはエスケープ処理しよう。 -->
+            <a class="nav-link" href="<?php echo esc_attr($item->url); ?>"><?php echo esc_html($item->title); ?></a>
           </li>
           <?php endforeach; ?>
         </ul>
